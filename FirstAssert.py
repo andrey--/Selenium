4fbaf5f1f0172f5f8631e6191d29bf30e169db7a
Andrey Kovalenko <kovalenko.andrey.odessa@gmail.com>
2019-07-14 18:12:42 +0300

    Add unittests, delete russian comments

---
+++ FirstAssert.py
@@ -0,0 +1,8 @@
+def twest_substring(full_string, substring):
+    # ваша реализация, напишите assert и сообщение об ошибке
+    assert substring in full_string, f"expected '{substring}' to be substring of '{full_string}'"
+
+twest_substring("fulltexst", "Ssss")
+
+
+
---
+++ abs_project.py
@@ -0,0 +1,33 @@
+import unittest
+from selenium import webdriver
+import time
+
+
+def method_name(url):
+    link = url
+    browser = webdriver.Chrome()
+    browser.get(link)
+    browser.find_element_by_css_selector(
+        "div.first_block>div.form-group>input[placeholder='Введите имя']").send_keys("Test")
+    browser.find_element_by_css_selector("div.first_block>div.form-group>input[placeholder='Введите фамилию']").send_keys("Test")
+    browser.find_element_by_css_selector("div.first_block>div.form-group>input[placeholder='Введите Email']").send_keys("Test")
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+    time.sleep(1)
+    welcome_text_elt = browser.find_element_by_tag_name("h1")
+    welcome_text = welcome_text_elt.text
+    return welcome_text
+
+class TestAbs(unittest.TestCase):
+
+    def test_abs1(self):
+        result=method_name("http://suninjuly.github.io/registration1.html")
+        self.assertEqual(result, "Поздравляем! Вы успешно зарегистировались!", "Should be absolute value of a number")
+    def test_abs2(self):
+        result=method_name("http://suninjuly.github.io/registration2.html")
+        self.assertEqual(result, "Поздравляем! Вы успешно зарегистировались!", "Should be absolute value of a number")
+
+
+
+if __name__ == "__main__":
+    unittest.main()
